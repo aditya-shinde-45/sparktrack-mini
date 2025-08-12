29a36f82e81ea99d5fb8e5b38dc623edab7a9d34
VS1230 <viveksj1230@gmail.com>
2025-08-12 16:24:49 +0530

    backend url changed

--- reviewpannel-frontend/src/Components/External/EvaluationForm.jsx
+++ reviewpannel-frontend/src/Components/External/EvaluationForm.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://sparktrack-mini.onrender.com/";
 
 const EvaluationForm = ({ groupId }) => {
   const [students, setStudents] = useState([]);
