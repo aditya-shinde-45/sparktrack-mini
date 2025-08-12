b8faa95fbe85167b46c876e06eb3ce6303d9f69f
ideablisss <ideabliss4@gmail.com>
2025-08-12 10:14:48 +0530

    delete

--- reviewpanel-backend/server.js
+++ reviewpanel-backend/server.js
@@ -21,7 +21,10 @@ const PORT = process.env.PORT || 5000;
 const TEST_TABLE = "pbl"; // Change this to an existing table
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://sparktrack-mini-lkij.vercel.app', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
