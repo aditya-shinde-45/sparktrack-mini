59e8603df796791bdc1e0f4872a89168ab32fdb3
ideablisss <ideabliss4@gmail.com>
2025-08-14 13:35:06 +0530

    bhai

--- reviewpannel-frontend/src/Pages/Common/Login.jsx
+++ reviewpannel-frontend/src/Pages/Common/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
       alert("Selected role is not supported for login.");
       return;
     }
-//chuitya
+//chuitya bhai
     try {
       const data = await apiRequest(endpoint, "POST", payload);
       localStorage.setItem("token", data.token);
