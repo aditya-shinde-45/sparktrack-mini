a038e74ae1719ce248a6997edc0d0edf67829f03
ideablisss <ideabliss4@gmail.com>
2025-08-19 02:25:46 +0530

    last push

--- reviewpanel-backend/Model/userModel.js
+++ reviewpanel-backend/Model/userModel.js
@@ -1,11 +1,10 @@
 import bcrypt from "bcryptjs";
 
-// Mock admin user (replace with DB later)
 export const users = [
   {
     id: 1,
-    username: "admin",
-    passwordHash: bcrypt.hashSync("admin123", 10), // hashed password
+    username: "Suresh.kapare@mituniversity.com",
+    passwordHash: bcrypt.hashSync("8698078603", 10), // hashed password
     role: "Admin",
   },
 ];
