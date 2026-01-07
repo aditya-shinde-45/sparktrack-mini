78e9b6040b8bff7fb9966469d7b24e2ed1ef1d0a
ideablisss <ideabliss4@gmail.com>
2026-01-08 03:09:14 +0530

    kuch nhi kiya

--- reviewpannel-frontend/src/Pages/Common/Home.jsx
+++ reviewpannel-frontend/src/Pages/Common/Home.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import Navbar from "../../Components/Common/Navbar";
 import Footer from "../../Components/Common/Footer";
 import HeroSection from "../../Components/Common/HeroSection";
-import AchievementsSection from "../../Components/Common/AchievementsSection";
-import DignitySection from "../../Components/Common/DignitySection";
+import AchievementsAndDignitySection from "../../Components/Common/AchievementsAndDignitySection";
 
 const Home = () => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="bg-gray-50 font-[Poppins]">
+      <div className="bg-gray-50 font-[Poppins] px-4 sm:px-6 lg:px-8">
         <HeroSection />
-        <AchievementsSection />
-        <DignitySection />
+        <AchievementsAndDignitySection />
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
